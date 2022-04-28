796b85ec355747b20ff51e81988c46f3ea5a5b20
VirsatusCodes <donavynpence@yahoo.com>
2022-04-27 22:02:49 -0700

    Light framework started put enough css and index to have a visual representation of what im doing and changing with my javascript, button and prompt for box sizing based on user input complete that whipes out old divs. trying to allow creation of divs from user, using a loop? whose value is used to create the new divs? as divs with the same name cant be made, right? look that up.

---
+++ java.js
@@ -0,0 +1,23 @@
+const gridContainer = document.querySelector('#gridContainer');
+const grid = document.createElement('div');
+grid.classList.add('grid');
+grid.textContent= " test";
+gridContainer.appendChild(grid);
+
+const userPrompt = document.querySelector('#userPrompt');
+userPrompt.addEventListener('click', () => {
+    createDivs()
+});
+
+//use a button to  throw up the prompt for grid size
+function createDivs () {
+    gridContainer.removeChild(grid);
+    userSelection =prompt("test");
+    console.log(userSelection)
+    for (let i = userSelection; i > 0 ; i--){
+        grid ;
+        grid.textContent= "userSelection";
+        gridContainer.appendChild(grid);
+    }
+ 
+}
