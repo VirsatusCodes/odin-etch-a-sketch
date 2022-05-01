78c4d82a2ba40243d9673a226cd20fc3eb036a4e
VirsatusCodes <donavynpence@yahoo.com>
2022-05-01 14:50:11 -0700

    completed the removeDivs function must now alter createDivs in such a way that it creates multiple layers based on input, maybe a for loop that adds line breaks and continuous generation.

--- java.js
+++ java.js
@@ -4,20 +4,39 @@ grid.classList.add('grid');
 grid.textContent= " test";
 gridContainer.appendChild(grid);
 
+const grid1 = document.createElement('div');
+grid1.classList.add('grid');
+grid1.textContent= " test";
+gridContainer.appendChild(grid1);
+
 const userPrompt = document.querySelector('#userPrompt');
 userPrompt.addEventListener('click', () => {
-    createDivs()
+    getUserSelection();
+    removeDivs();
+    createDivs();
 });
 
-//use a button to  throw up the prompt for grid size
+//you cant normally create multiple divs like this, however
+//i guess because of interactions with scope if its in a function
+// you can create  new div's using the same variable, OR you can 
+//do the initial declaration using "let" instead of "const" and you
+//can continue to create without using a function.
+
+function getUserSelection() {
+    userSelection = prompt("test");
+}
+
+function removeDivs() {
+    while (gridContainer.firstChild) {
+        gridContainer.removeChild(gridContainer.firstChild);
+    }
+}
+
 function createDivs () {
-    gridContainer.removeChild(grid);
-    userSelection =prompt("test");
-    console.log(userSelection)
     for (let i = userSelection; i > 0 ; i--){
         const grid = document.createElement('div');
         grid.classList.add("grid");
-        grid.textContent = `${i} /` ;
+        grid.textContent = `${i}/` ;
         gridContainer.appendChild(grid);   
     }
  
